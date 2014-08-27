edf630e5875813aab2fa0db86dbafbb07224dd1b
Louis Borsu <sat@satprod.net>
2014-08-27 13:16:37 +0200

    HTTP Get 2controllers

--- js/controller/ServicesCtrl.js
+++ js/controller/ServicesCtrl.js
@@ -1,20 +1,24 @@
-app.factory('BasicInfo', function(){
+app.factory('BasicInfo', function($http, $q){
 
     var basicInfo = {
 
-        basicInfo : {
-            "phonePolice" : "122",
-            "phoneFiremen" : "100",
-            "phoneConcierge" : "027777777",
-            "wifiName" : "AA4_Wifi",
-            "wifiPass" : "bonaldokeijs",
-            "maidNextDate" : "26/4/2013 - 09:00",
-            "maidNextDay" : "Tuesday",
-            "maidNextTime" : "15:00",
-            "maidPhoneNumber" : "0488888888"
-        },
+        basicInfo : false,
+
         getBasicInfo : function(){
-            return basicInfo.basicInfo;
+            var deferred = $q.defer();
+            if (basicInfo.basicInfo !== false){
+                deferred.resolve(basicInfo.basicInfo);
+            }else{
+                $http({method: 'GET', url: 'http://www.pvimmo.com/royaltest/basicInfo.json'}).
+                    success(function(data, status, headers, config) {
+                        basicInfo.basicInfo = data;
+                        deferred.resolve(basicInfo.basicInfo);
+                    }).
+                    error(function(data, status, headers, config) {
+                        deferred.reject('Network Problem!!');
+                    });
+            } 
+            return deferred.promise;
         }
     };
     return basicInfo;
@@ -24,9 +28,11 @@ app.factory('BasicInfo', function(){
 app.controller('ServicesCtrl', function($scope,$rootScope,BasicInfo){
 
     $scope.panelServiceBasic = function(){
-        $scope.info = BasicInfo.getBasicInfo();
-        $rootScope.servicesPanel  = 1;
-        $scope.loader = false;
+        $scope.info = BasicInfo.getBasicInfo().then(function(info){
+            $scope.info = info;
+            $rootScope.servicesPanel  = 1;
+            $scope.loader = false;
+        });
     };
 
     $scope.panelServiceClassic = function(){
@@ -35,24 +41,30 @@ app.controller('ServicesCtrl', function($scope,$rootScope,BasicInfo){
     };
 
     $scope.panelServiceMaid = function(){
-        $scope.info = BasicInfo.getBasicInfo();
-        $rootScope.servicesPanel  = 3;
-        $rootScope.maidPanel  = 0;
-        $scope.loader = false;
+        $scope.info = BasicInfo.getBasicInfo().then(function(info){
+            $scope.info = info;
+            $rootScope.servicesPanel  = 3;
+            $rootScope.maidPanel  = 0;
+            $scope.loader = false;
+        });
     };
 
     $scope.panelServiceMaidAddDate = function(){
-        $scope.info = BasicInfo.getBasicInfo();
-        $rootScope.servicesPanel  = 3;
-        $rootScope.maidPanel  = 1;
-        $scope.loader = false;
+        $scope.info = BasicInfo.getBasicInfo().then(function(info){
+            $scope.info = info;
+            $rootScope.servicesPanel  = 3;
+            $rootScope.maidPanel  = 1;
+            $scope.loader = false;
+        });
     };
 
     $scope.panelServiceMaidChangeDay = function(){
-        $scope.info = BasicInfo.getBasicInfo();
-        $rootScope.servicesPanel = 3;
-        $rootScope.maidPanel  = 2;
-        $scope.loader = false;
+        $scope.info = BasicInfo.getBasicInfo().then(function(info){
+            $scope.info = info;
+            $rootScope.servicesPanel = 3;
+            $rootScope.maidPanel  = 2;
+            $scope.loader = false;
+        });
     };
 
     $scope.panelServicePrivate = function(){
